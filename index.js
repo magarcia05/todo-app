480789696c16c7c51d0fbcbc4f7654489ee8723b
Miguel <magarcia05@icloud.com>
2018-06-20 05:35:36 +0000

    POST route created to insert new todos to the mongo db

--- index.js
+++ index.js
@@ -6,15 +6,19 @@
 
 var express = require('express'),
     app = express(),
+    bodyParser = require('body-parser'),
     port = process.env.PORT || 3000;
 
 var todoRoutes = require('./routes/todos'); // Get the todo routes from location
 
+//We need this in order to handle the post request data from the user
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 // routes ======================================================================
 
 app.get('/', (req, res) => {
-  res.send('Hello from the root router');
+    res.send('Hello from the root router');
 });
 
 app.use('/api/todos', todoRoutes);
--- routes/todos.js
+++ routes/todos.js
@@ -2,20 +2,30 @@
  * Todos.js stores all the existing routes from our todos api
  * They will be called by the user as follows: /api/todos/[userInput]
  */
- 
+
 var express = require('express'),
     router = express.Router(),
-    db = require("../models");  //Get the database from models
+    db = require("../models"); //Get the database from models
 
-router.get('/', function(req, res){
-  db.Todo.find()
-    .then((todos) => {
-        res.json(todos);
-    })
-    .catch((err) => {
-        res.send(err);
-    })
+
+router.get('/', (req, res) => {
+    db.Todo.find()
+        .then((todos) => {
+            res.json(todos);
+        })
+        .catch((err) => {
+            res.send(err);
+        });
 });
 
+router.post('/', (req, res) => {
+    db.Todo.create(req.body)
+        .then((newTodo) => {
+            res.json(newTodo);
+        })
+        .catch((err) => {
+            res.send(err);
+        });
+});
 
 module.exports = router;
