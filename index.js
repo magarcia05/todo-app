cc3565d11dd2cea8e073770d1c74e95550a31685
Miguel <magarcia05@icloud.com>
2018-06-21 02:39:35 +0000

    Created Bare bones for the front end side of the web app

--- index.js
+++ index.js
@@ -15,14 +15,23 @@ var todoRoutes = require('./routes/todos'); // Get the todo routes from location
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+//This is in order to serve our content from views
+app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/views'));
+
+
+
 // routes ======================================================================
 
 app.get('/', (req, res) => {
-    res.send('Hello from the root router');
+    res.sendFile('index.html');
 });
 
 app.use('/api/todos', todoRoutes);
 
+app.get('*', (req, res) => {
+    res.redirect('/');
+});
 
 // listen ======================================================================
 
