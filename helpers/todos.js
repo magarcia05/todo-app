9d4055345986a7162a4b439f54795b2f1f4349b4
Miguel <magarcia05@icloud.com>
2018-06-23 20:54:39 +0000

    Creaded jQuery CRUD functions for TodoList

--- helpers/todos.js
+++ helpers/todos.js
@@ -7,72 +7,72 @@
 var db = require('../models');
 
 exports.getTodos = (req, res) => {
-    db.Todo.find()
-        .then((todos) => {
-            res.status(200).json(todos);
-        })
-        .catch((err) => {
-            //500 Internal Server Error
-            res.status(500).send(err);
-        });
+  db.Todo.find()
+    .then((todos) => {
+      res.status(200).json(todos);
+    })
+    .catch((err) => {
+      //500 Internal Server Error
+      res.status(500).send(err);
+    });
 };
 
 exports.createTodo = (req, res) => {
-    db.Todo.create(req.body)
-        .then((newTodo) => {
-            //201 Created success
-            res.status(201).json(newTodo);
-        })
-        .catch((err) => {
-            //500 Internal Server Error
-            res.status(500).send(err);
-        });
+  db.Todo.create(req.body)
+    .then((newTodo) => {
+      //201 Created success
+      res.status(201).json(newTodo);
+    })
+    .catch((err) => {
+      //500 Internal Server Error
+      res.status(500).send(err);
+    });
 };
 
 exports.getTodo = (req, res) => {
-    db.Todo.findById(req.params.todoId)
-        .then((todo) => {
-            //200 OK
-            res.status(200).json(todo);
-        })
-        .catch((err) => {
-            //500 Internal Server Error
-            res.status(500).send(err);
-        });
+  db.Todo.findById(req.params.todoId)
+    .then((todo) => {
+      //200 OK
+      res.status(200).json(todo);
+    })
+    .catch((err) => {
+      //500 Internal Server Error
+      res.status(500).send(err);
+    });
 };
 
 exports.updateTodo = (req, res) => {
-    db.Todo.findByIdAndUpdate(
-            // the id of the item to find
-            req.params.todoId,
+  db.Todo.findByIdAndUpdate(
+      // the id of the item to find
+      req.params.todoId,
 
-            // the change to be made. Mongoose will smartly combine your existing 
-            // document with this change, which allows for partial updates too
-            req.body,
+      // the change to be made. Mongoose will smartly combine your existing 
+      // document with this change, which allows for partial updates too
+      req.body,
 
-            // an option that asks mongoose to return the updated version 
-            // of the document instead of the pre-updated one.
-            { new: true }
-        )
-        .then((todo) => {
-            //200 OK
-            res.status(200).json(todo);
-        })
-        .catch((err) => {
-            //500 Internal Server Error
-            res.status(500).send(err);
-        });
+      // an option that asks mongoose to return the updated version 
+      // of the document instead of the pre-updated one.
+      { new: true }
+    )
+    .then((todo) => {
+      //200 OK
+      res.status(200).json(todo);
+    })
+    .catch((err) => {
+      //500 Internal Server Error
+      res.status(500).send(err);
+    });
 };
 
 exports.deleteTodo = (req, res) => {
-    db.Todo.findByIdAndRemove(req.params.todoId)
-        .then((todo) => {
-            res.status(200).json(todo);
-        })
-        .catch((err) => {
-            //500 Internal Server Error
-            res.status(500).send(err);
-        });
+  db.Todo.findByIdAndRemove(req.params.todoId)
+    .then((todo) => {
+      res.status(200).json(todo);
+    })
+    .catch((err) => {
+      //500 Internal Server Error
+      res.status(500).send(err);
+    });
 };
 
 module.exports = exports;
--- public/app.js
+++ public/app.js
@@ -1,1 +1,88 @@
-alert("Javascript");
+/* global $ */
+$(document).ready(function() {
+  $.getJSON("/api/todos")
+    .then(addTodos);
+
+  $('#todoInput').keypress(function(event) {
+    if (event.which == 13) {
+      createTodo();
+    }
+  });
+
+  //Workaround to select the spans inside the list-group class
+  $('.list-group').on('click', 'li', function(event){
+    updateTodo($(this));
+  });
+
+  //Workaround to select the spans inside the list-group class
+  $('.list-group').on('click', 'span', function(event){
+    //Event continue to trigger up, so it goes to .list-grou- -> li
+    event.stopPropagation();
+    removeTodo($(this).parent());
+  });
+});
+
+/*****************************CRUD functions for Todolist *****************************/
+
+function addTodo(todo) {
+  var newTodo = $('<li>'+todo.name +' <span>X</span></li>');
+  newTodo.data('id', todo._id);
+  newTodo.data('completed', todo.completed);
+  newTodo.addClass('list-group-item');
+
+  $('.list-group').append(newTodo);
+}
+
+function addTodos(todos) {
+  //add Todos to the page
+  todos.forEach(function(todo) {
+    addTodo(todo);
+  });
+}
+
+function createTodo() {
+  //send request to create new todo
+  var usrInput = $('#todoInput').val();
+  $.post('/api/todos', { name: usrInput })
+    .then(function(newTodo) {
+      //Remove the Todotask from the form
+      $('#todoInput').val('');
+      //Display the Todo we just added
+      addTodo(newTodo);
+    })
+    .catch(function(err) {
+      console.log(err);
+    });
+}
+
+function updateTodo(todo){
+  var updateUrl = '/api/todos/' + todo.data('id');
+  var isDone = !todo.data('completed');
+  var updateData = {completed: isDone}
+  $.ajax({
+    method: 'PUT',
+    url: updateUrl,
+    data: updateData
+  })
+  .then(function(updatedTodo){
+    console.log(updateData);
+    todo.toggleClass("done");
+    todo.data('completed', isDone);
+  });
+}
+
+function removeTodo(todo){
+  var deleteUrl = '/api/todos/' + todo.data('id'); 
+  
+  $.ajax({
+    method: 'DELETE',
+    url: deleteUrl
+  })
+  .then(function(data){
+    //$(this).parent().remove() to remove it from the screen
+    todo.remove();
+  })
+  .catch(function(err){
+    console.log(err);
+  });
+}
